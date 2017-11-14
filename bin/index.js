2b16635b6275f78fcaf1ac71dbeb306d97f2b292
Denis Florkin <denisflorkin@gmail.com>
2017-11-14 23:31:19 +0100

    better

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,26 @@
 #! /usr/bin/env node
 const exec = require('child_process').exec
+const path = require('path')
+const command = [...process.argv].slice(2).join(' ')
 
-const command = process.argv[2]
 console.log([...process.argv])
 console.log('cwd :', process.cwd())
+console.log('running npm run ' + command)
 
-const cmd = exec('npm run ' + command, function (err, stdout, stderr) {
+// $(npm root)
+exec('npm root', function (err, stdout, stderr) {
   if (err) {
     console.warn(err)
     throw new Error(err)
   }
-  console.log(stderr)
-  console.log(stdout)
+
+  const rootCWS = path.resolve(stdout, '../')
+  console.log('rootCWS :', rootCWS)
+  exec(`npm run ${command}`, { cwd: rootCWS }, function (err, stdout, stderr) {
+    if (err) {
+      console.warn(err)
+      throw new Error(err)
+    }
+    console.log(stderr, stdout)
+  });
 });
