744ac1a0c8fef5f2a88d28cbeccdcbb15a549faa
Denis Florkin <denisflorkin@gmail.com>
2017-11-14 23:09:08 +0100

    update

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,15 @@
-"use strict";
+#! /usr/bin/env node
+const exec = require('child_process').exec
 
-require = require("@std/esm")(module, true)
-module.exports = require("./main").default
+const command = process.argv[2]
+console.log([...process.argv])
+console.log('cwd :', process.cwd())
+
+const cmd = exec('npm run ' + command, function (err, stdout, stderr) {
+  if (err) {
+    console.warn(err)
+    throw new Error(err)
+  }
+  console.log(stderr)
+  console.log(stdout)
+});
