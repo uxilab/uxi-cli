45800c97c0900b90f478797fecc93322e966ff05
Denis Florkin <denisflorkin@gmail.com>
2017-11-14 23:59:42 +0100

    up

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,43 @@
 #! /usr/bin/env node
 const exec = require('child_process').exec
 const path = require('path')
-const command = [...process.argv].slice(2).join(' ')
+
+
+const cmds = {
+  test: 'jest'
+}
+const commandArg = process.argv[2]
+const command = cmds[commandArg]
+if (!command) {
+  throw new Error('command not supported "' + commandArg +'"')
+}
+const args = [...process.argv].slice(3).join(' ')
 
 console.log([...process.argv])
 console.log('cwd :', process.cwd())
 console.log('running npm run ' + command)
 
-// $(npm root)
 exec('npm root', function (err, stdout, stderr) {
   if (err) {
     console.warn(err)
     throw new Error(err)
   }
 
-  const rootCWS = path.resolve(stdout, '../')
-  console.log('rootCWS :', rootCWS)
-  exec(`npm run ${command}`, { cwd: rootCWS }, function (err, stdout, stderr) {
+  const rootCWD = path.resolve(stdout, '../')
+  console.log('rootCWD :', rootCWD)
+
+  // $(npm root)
+  const cmd = exec(
+      `${command} ${args}`,
+      { cwd: rootCWD },
+      function (err, stdout, stderr) {
     if (err) {
       console.warn(err)
       throw new Error(err)
     }
-    console.log(stderr, stdout)
+    console.warn(err)
   });
-});
+  process.stdout.pipe(cmd.stdout)
+  process.stderr.pipe(cmd.stderr)
+
+})
