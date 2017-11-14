2aeeb623efb8a1d7dc1266bedd55bf1787428cda
Denis Florkin <denisflorkin@gmail.com>
2017-11-15 00:21:13 +0100

    better + patch

--- bin/index.js
+++ bin/index.js
@@ -4,18 +4,18 @@ const path = require('path')
 
 
 const cmds = {
-  test: 'jest'
+  test: 'node_modules/.bin/jest'
 }
 const commandArg = process.argv[2]
 const command = cmds[commandArg]
 if (!command) {
-  throw new Error('command not supported "' + commandArg +'"')
+  throw new Error('command not supported "' + commandArg + '"')
 }
 const args = [...process.argv].slice(3).join(' ')
 
 console.log([...process.argv])
 console.log('cwd :', process.cwd())
-console.log('running npm run ' + command)
+console.log('running' + command)
 
 exec('npm root', function (err, stdout, stderr) {
   if (err) {
@@ -24,20 +24,22 @@ exec('npm root', function (err, stdout, stderr) {
   }
 
   const rootCWD = path.resolve(stdout, '../')
+  console.log(`${command} ${args}`, 'running in', rootCWD)
   console.log('rootCWD :', rootCWD)
 
   // $(npm root)
   const cmd = exec(
-      `${command} ${args}`,
-      { cwd: rootCWD },
-      function (err, stdout, stderr) {
-    if (err) {
-      console.warn(err)
-      throw new Error(err)
-    }
-    console.warn(err)
-  });
-  process.stdout.pipe(cmd.stdout)
-  process.stderr.pipe(cmd.stderr)
+    `${command} ${args}`,
+    { cwd: rootCWD },
+    function (err, stdout, stderr) {
+      if (err) {
+        console.warn(err)
+        throw new Error(err)
+      }
+      console.warn(stderr)
+      process.exit(0)
+    });
+  cmd.stdout.pipe(process.stdout)
+  cmd.stderr.pipe(process.stderr)
 
 })
