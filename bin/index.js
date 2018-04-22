9b9aeed461b7dd7b06917c9b0b3538170ad44918
Denis Florkin <denisflorkin@gmail.com>
2018-04-22 19:24:07 +0200

    wip clean + doc

--- bin/index.js
+++ bin/index.js
@@ -3,67 +3,40 @@ const exec = require('child_process').exec;
 const path = require('path');
 // const npmRun = require('npm-run');
 const pkg = require('../package.json');
-// const readline = require('readline')
 
 process.stdin.setEncoding('utf8');
 
 const cmds = pkg.commands;
+const cmdsArgs = pkg.commandsArgs;
 
 const commandArg = process.argv[2];
 const command = cmds[commandArg];
 if (!command) {
-  console.log(`command not supported "${commandArg === undefined ? '' : commandArg}"
+  console.log(`
+  ci-ops version: ${pkg.version}
 
   List of available commands:
-${Object.keys(cmds).map(c => `  - ci-ops ${c}`).join('\n')}
+
+${Object.keys(cmds).map((c, i) =>
+    `    - ci-ops ${c} ${cmdsArgs[c]}`).join('\n')}
 `);
+  process.exit(1);
 }
 const args = [...process.argv].slice(3).join(' ');
 
-/*
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'OHAI> ',
-
-});
-rl.prompt();
-rl.on('line', (input) => {
-  console.log(`Received: ${input}`);
-});
-*/
-
-// console.log([...process.argv]);
-// console.log('cwd :', process.cwd());
-// console.log(`running${command}`);
-
-exec('npm root', (err, stdout, stderr) => {
+exec('npm root', (err, stdout/* , stderr */) => {
   if (err) { throw new Error(err); }
 
   const rootCWD = path.resolve(stdout, '../');
-  // console.log(`${command} ${args}`, 'running in', rootCWD)
-  // console.log('rootCWD :', rootCWD)
-
   const cmd = exec(
     `${command} ${args}`,
-    { cwd: rootCWD },
+    { cwd: rootCWD, stdio: inherit },
     (err, stdout, stderr) => {
       if (err) { console.warn(err); }
       if (stderr) { console.warn(stderr); }
       console.log(stdout);
       process.exit(0);
     });
-
-  // const rl = readline.createInterface({
-  //   input: cmd.stdin,
-  //   output: cmd.stdin,
-  //   prompt: 'OHAI> '
-
-  // });
-  // rl.prompt();
-  // rl.on('line', (input) => {
-  //   console.log(`Received: ${input}`);
-  // });
 
   const stdin = process.openStdin();
   stdin.addListener('data', (d) => {
@@ -82,5 +55,4 @@ exec('npm root', (err, stdout, stderr) => {
 
   cmd.stdout.pipe(process.stdout);
   cmd.stderr.pipe(process.stderr);
-  // cmd.stdin.pipe(process.stdin);
 });
---
+++ readme.md
@@ -0,0 +1,73 @@
+# ci-ops
+
+jest + eslint + webpack + babel + webpack-dev-server
+
+  - jest + enzyme + snapshot
+  - eslint (airbnb)
+  - webpack 4
+  - webpack-dev-server
+  - babel, babel-loader:
+    - latest ('env')
+    - react jsx
+    - object spread
+
+## install:
+- npm i ci-ops
+
+print help:
+
+`$ node_modules/.bin/ci-ops`:
+
+
+## Usage:
+
+in the package.json:
+```JSON
+  "scripts": {
+    "build": "ci-ops build",
+    "lint": "ci-ops lint",
+    "test": "ci-ops test",
+  }
+```
+
+## Commands:
+  - **test**
+  - **test:watch**
+  - **test:coverage**
+  - **lint**
+  - **build**
+  - **build:react**
+  - **dev**
+
+
+## Default behaviour:
+
+- **test** scans a target directory (default to .src/) for files under a folder named *\_\_test\_\_* or files that end in *.test.js*
+
+- **test** and **lint** default to targeting ./src folder, but you can say ortherwise by passing the path as the first argument:
+
+  `$ ci-ops lint ./myFolder` or `$ ci-ops test ./source`
+
+- **build** defaults to using ./src/index.js as source and .build/app.js as output
+
+    To overwrire webpack config, or some part of it, create you config file:
+    ```js
+    // webpack.myconf.js
+    const webpack = require('webpack');
+    const path = require('path');
+
+    module.exports = {
+      entry: [
+        './mySrcFolder/myEntryPoint.js',
+      ],
+      output: {
+        path: path.join(__dirname, 'myOutputFolder'),
+        filename: 'myOutputFileName.js',
+      },
+    };
+
+    ```
+
+    then use the config,
+
+    `$ ci-ops build --config webpack.myconf.js`
