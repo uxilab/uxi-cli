e9f633c76fef8ead03741d622668c5b12ca5370c
Denis Florkin <denisflorkin@gmail.com>
2018-05-04 10:40:25 +0200

    add transform-class-properties

--- jest.transform.js
+++ jest.transform.js
@@ -1,6 +1,6 @@
 const babelOptions = {
   "presets": ["env", "react"],
-  "plugins": ["transform-object-rest-spread"]
+  "plugins": ["transform-object-rest-spread", "transform-class-properties"]
 };
 
 module.exports = require('babel-jest').createTransformer(babelOptions);
